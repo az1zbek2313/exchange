26c68b08fba1cae3df6231916495562f7aacdeec
Azizbek:04 <150643944+az1zbek2004@users.noreply.github.com>
2024-04-19 01:35:02 +0500

    biroz o'zgarishlar kiritildi

--- src/components/Currency/index.tsx
+++ src/components/Currency/index.tsx
@@ -5,12 +5,24 @@ import {useEffect, useState} from "react";
 import { useRef } from 'react'
 import data from "../../assets/currency-flags.json";
 
+type ConvertProps = {
+    base: string,
+    results: {
+        AFN:number
+    }
+    updated: string,
+    ms: number
+}
+
 
 function Currency() {
     const [quantity, setQuantity] = useState<number>(localStorage.getItem("amount")?Number(localStorage.getItem("amount")):0);
     const [exchange, setExchange] = useState<boolean>(false);
-    const [convert, setConvert] = useState("");
-    const [values, setValue] = useState<string>("AF");
+    const [convert, setConvert] = useState<ConvertProps[] | any>(null);
+    const [country, setCountry] = useState<number>(0);
+    const [decimalPart, setDecimal] = useState<string>("0.00");
+    const [whole, setWhole] = useState<string>("");
+    const [values, setValue] = useState<string>("AFN");
     const amount = useRef<HTMLInputElement>(null);
     const from = useRef<HTMLSelectElement>(null);
     const to = useRef<HTMLSelectElement>(null);
@@ -28,11 +40,20 @@ function Currency() {
     // EVENTS
     function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
         e.preventDefault();
-        if (validate(Number(amount.current?.value))) {
-            setQuantity(Number(amount.current?.value));
-            localStorage.setItem("amount", JSON.stringify(amount));
+        const inputValue = Number(amount.current?.value); // Extracting the value
+        if (validate(inputValue)) {
+            setQuantity(inputValue);
+            localStorage.setItem("amount", String(inputValue)); // Storing the value as a string
+            const son = !exchange ? Number(1/qiymat) * inputValue : Number(qiymat) * inputValue ; 
+            const son1 = exchange ? Number(1/qiymat) * inputValue : Number(qiymat) * inputValue ; 
+            const qoldiq:number = !exchange ? (son % 1) : (son1 % 1);
+            const decimalPart = qoldiq.toString().substring(4, 8);
+            const whole = Math.trunc(son) +'.' + qoldiq.toString().substring(2, 4);
+            setDecimal(decimalPart);
+            setWhole(whole);
         }
     }
+    
 
     function handleExchange() {
         exchange ? setExchange(false) : setExchange(true);        
@@ -41,10 +62,37 @@ function Currency() {
     function handleChange1(e:  React.ChangeEvent<HTMLSelectElement>) {
         setValue(e.target?.value);        
     }
+
+    const currencyKeys = convert?.results && Object.keys(convert?.results);
+    const currencyValues = convert?.results && Object.values(convert?.results);
+
+    useEffect(() => {
+    const ind = currencyKeys && currencyKeys.findIndex((el:any) => el === values);
+    if (ind !== -1) {
+        setCountry(ind);
+    }
+    }, [currencyKeys, values]);
+    const qiymat = currencyValues && currencyValues.find((el:any, index:number) => {
+        return el && country == index
+    })    
+
     
-    console.log(values);
+    const symbole = data.find(el => el.currency?.code == values);
     
     useEffect(() => {
+    const son = !exchange ? Number(1/qiymat) * quantity : Number(qiymat) * quantity ; 
+    const son1 = exchange ? Number(1/qiymat) * quantity : Number(qiymat) * quantity ; 
+    const qoldiq:number = !exchange ? (son % 1) : (son1 % 1);
+    const decimalPart = qoldiq.toString().substring(4, 8);
+    const whole = Math.trunc(son) +'.' + qoldiq.toString().substring(2, 4);
+    setDecimal(decimalPart);
+    setWhole(whole);
+    console.log(quantity);
+}, [quantity, exchange, qiymat]);
+
+    
+    useEffect(() => {
+
         fetch("https://api.fastforex.io/fetch-all?api_key=a58c7e9b7a-988376f8c5-sc54ik")
             .then(res => res.json())
             .then(data => {
@@ -52,10 +100,9 @@ function Currency() {
             })
             .catch(err => {
                 console.log(err);
-            })
+            });
+        
     }, [])
-    
-    
     
 
   return (
@@ -72,27 +119,35 @@ function Currency() {
 
                 <div className="currency__input">
                     <label htmlFor="Amount">Amount</label>
-                    <input ref={amount} type="number" placeholder={`sum`}/>
+                    <input defaultValue={quantity} ref={amount} type="number" placeholder={!exchange ? `${symbole?.currency.symbol ? symbole?.currency.symbol : symbole?.currency.code}`:"$"}/>
                 </div>
 
                 <div className="currency__input">
                     <label htmlFor="Amount">
                         <span>From</span> 
-                        {
-                                data && data.map((el, index) => {
-                                    return el.code == values && <img key={index} width={32} src={el.flag} alt="flag icon" />
-                                })
-                            }
+                       {
+                        !exchange ? 
+                            data && data.map((el, index) => {
+                                return el.currency.code == values && <img key={index} width={32} src={el.flag ? el.flag : uz} alt="flag icon" />
+                            }) :
+                            <img width={32} src={us} alt="flag icon" />
+                       }
                     </label>
+                       {
+                        !exchange ?
                         <select onChange={handleChange1} ref={from} name="currency" id="Amount">
                             {
                                 data && data.map((el, index) => {
-                                    return <option key={index} value={`${el.code}`}>
+                                    return <option key={index} value={`${el.currency.code}`}>
                                         {el.currency.name + " " + el.currency.code}
                                     </option>
                                 })
                             }
+                        </select>:
+                        <select ref={from} name="currency" id="Amount">
+                            <option value="US">US Dollars</option>
                         </select>
+                        }
                 </div>
 
                 <div onClick={handleExchange} className="currency--exchange">
@@ -102,22 +157,48 @@ function Currency() {
                 <div className="currency__input">
                     <label htmlFor="Amount">
                         <span>To</span> 
-                        <img width={32} src={us} alt="flag icon" />
+                        {
+                            !exchange ? 
+                            <img width={32} src={us} alt="flag icon" />
+                            :
+                            data && data.map((el, index) => {
+                                return el.currency.code == values && <img key={index} width={32} src={el.flag ? el.flag : uz} alt="flag icon" />
+                            })
+                        }
                     </label>
+                       {
+                        !exchange ?
                         <select ref={to} name="currency" id="Amount">
                             <option value="US">US Dollars</option>
-                        </select>
+                        </select> :
+                         <select onChange={handleChange1} ref={to} name="currency" id="Amount">
+                         {
+                             data && data.map((el, index) => {
+                                 return <option key={index} value={`${el.currency.code}`}>
+                                     {el.currency.name + " " + el.currency.code}
+                                 </option>
+                             })
+                         }
+                     </select>
+                        }
                 </div>
                 
               </form>
 
               <div className="currency__priceChange">
-                <h5>{`${quantity}`}.00 Uzbekistani Sums =</h5>
-                <h1>0.00<span className="faded-digits">007988</span> US Dollars</h1>
-                 <div>
-                    <p>1 UZS = 0.0000790852 USD</p>
-                    <p>1 USD = 12,644.6 UZS</p>
+                <h5>{`${quantity}`}.00 {!exchange ? (data && data.map(el => (values == el.currency.code && el.currency.name))):"United State Dollar"} =</h5>
+                <h1>{whole}<span className="faded-digits">{decimalPart && decimalPart}</span> US Dollars</h1>
+                 {
+                    !exchange ?
+                    <div>
+                    <p>1 {values} = {1 / qiymat} USD</p>
+                    <p>1 USD = {qiymat} {values}</p>
+                </div>:
+                <div>
+                    <p>1 USD = {qiymat} {values}</p>
+                    <p>1 {values} = {1 / qiymat} USD</p>
                 </div>
+                 }
               </div>
             </div>
   )
